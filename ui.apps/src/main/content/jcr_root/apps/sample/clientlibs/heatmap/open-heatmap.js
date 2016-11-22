8f01f45d6b0a69ae1226963afcf8f520447d6590
Sridhar Jayakumar <jayakuma@adobe.com>
2016-11-22 14:06:06 -0800

    Initial JS Version printing path

--- ui.apps/src/main/content/jcr_root/apps/sample/clientlibs/heatmap/open-heatmap.js
+++ ui.apps/src/main/content/jcr_root/apps/sample/clientlibs/heatmap/open-heatmap.js
@@ -1,18 +1,5 @@
 /*
- * ADOBE CONFIDENTIAL
- *
- * Copyright 2012 Adobe Systems Incorporated
- * All Rights Reserved.
- *
- * NOTICE:  All information contained herein is, and remains
- * the property of Adobe Systems Incorporated and its suppliers,
- * if any.  The intellectual and technical concepts contained
- * herein are proprietary to Adobe Systems Incorporated and its
- * suppliers and may be covered by U.S. and Foreign Patents,
- * patents in process, and are protected by trade secret or copyright law.
- * Dissemination of this information or reproduction of this material
- * is strictly forbidden unless prior written permission is obtained
- * from Adobe Systems Incorporated.
+ * Custom JavaScript to open a heatmap URL on a different browser window2
  */
 
 (function(document, $) {
@@ -20,13 +7,7 @@
     $(document).on("click", ".open-heatmap", function(e){
     	
         var path = Granite.HTTP.internalize(window.location.pathname);
-
-        if( href ) {
             alert(path);
-            e.preventDefault();
-            e.stopImmediatePropagation();
-            e.stopPropagation();
-        }
     });
 
     // workaround to not show the switcher on touch devices (TO BE REMOVED) 
